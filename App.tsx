ededd04de71c28a5d2ea849521de68bc309e541c
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-06-14 03:56:11 +0800

    Implement json server and fetch collection

--- App.tsx
+++ App.tsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { StyleSheet, SafeAreaView, Text } from 'react-native'
 
+import { Item } from '@customtypes/item'
+import api from '@constants/api'
+
 const App: React.FC = () => {
+  const [collection, setCollection] = useState<Item[] | []>([])
+
+  useEffect(() => {
+    const fetchCollection = async (): Promise<void> => {
+      const response = await fetch(`${api}/data`)
+      const data = (await response.json()) as Item[]
+      setCollection(data)
+      console.log('data', data)
+    }
+
+    if (!collection.length) {
+      console.log('collection', collection)
+      fetchCollection()
+    }
+  }, [collection])
+
   return (
     <SafeAreaView style={styles.container}>
       <Text style={styles.title}>Carousel</Text>
