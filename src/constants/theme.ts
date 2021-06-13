0b98b7d58c001ee00317ce59c5c75c701fba4fa8
Andrey Ramirez <ramirez.jag00@gmail.com>
2021-06-13 21:49:39 +0800

    Add color themes

---
+++ src/constants/theme.ts
@@ -0,0 +1,19 @@
+import { Theme, Colors } from '@customtypes/theme'
+
+const colors: Colors = {
+  $white: '#ffffff',
+  $mercury: '#eaeaea',
+  $boulder: '#7a7a7a',
+  $mineShaft: '#202020',
+  $black: '#000000',
+  $vistaWhite: '#F9F1F0',
+  $azalea: '#FADCD9',
+  $roseBud: '#F8AFA6',
+  $geraldine: '#F79489',
+}
+
+const theme: Theme = {
+  colors,
+}
+
+export { theme as default, colors }
